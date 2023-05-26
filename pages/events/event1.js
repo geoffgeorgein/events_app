315f53bb92804871a3d56a248f3df5cbcd865807
Geoff George <geoffgeorgethommana@gmail.com>
2023-05-26 12:56:13 +0530

    routes

---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+const Page=()=> {
+    return (
+      <h1> Events</h1>
+    )
+  }
+
+  export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,32 @@
+const Page=()=> {
+    return (
+
+        <div>
+
+            <h1> Events Page</h1>
+
+                <a href='/'>
+                <h2>Events in London</h2>
+                </a>
+
+                <a href='/'>
+                <h2>Events in Paris</h2>
+                </a>
+
+                <a href='/'>
+                <h2>Events in NY</h2>
+                </a>
+
+
+                =
+
+                
+               
+
+
+        </div>
+      
+    )
+  }
+
+  export default Page;
