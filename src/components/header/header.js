520f2932d838446f36c3f5eb2b27f77652747d51
Geoff George <geoffgeorgethommana@gmail.com>
2023-05-29 18:14:32 +0530

    styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,15 +5,28 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
-    <nav>
+    <>
+    
+      
+        <header>
+        <div>
+          <div className='topNav'>
+            <Image alt='logo' src={'/images/logo_black.png'} height={50} width={50}></Image>
+            <nav>
 
-      <Link href='/'>Home</Link>
+              <Link href='/'>Home</Link>
 
-      <Link href='/events'>Events</Link>
-      <Link href='/about-us'>About Us</Link>
-    </nav>
-  </header>
+              <Link href='/events'>Events</Link>
+              <Link href='/about-us'>About Us</Link>
+            </nav>
+
+            
+            </div>
+            <p className="title"> Sed ut perspiciatis unde omnis</p>
+          </div>
+        </header>
+      </>
+  
   )
 }
 
