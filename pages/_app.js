e6f207cea273bee10c4a1a479b726f9d23b0a170
Geoff George <geoffgeorgethommana@gmail.com>
2023-05-29 17:20:54 +0530

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import MainLayout from '@/src/components/layout/main-layout'
-import '@/styles/globals.css'
+
+import '../styles/globals.css';
+import '../styles/general.sass';
 
 export default function App({ Component, pageProps }) {
 
