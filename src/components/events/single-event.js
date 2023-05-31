15ac6f9ca19c0e5da9ed934464a55384d69ea798
Geoff George <geoffgeorgethommana@gmail.com>
2023-05-31 22:49:58 +0530

    api routING

--- src/components/events/single-event.js
+++ src/components/events/single-event.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const SingleEvent = ({data}) => {
 
+  const inputemail=useRef();
+
+  const router=useRouter();
+
+  const Onsubmit=async(e)=>{
+    e.preventDefault();
+
+    const emailValue=inputemail.current.value;
+    console.log(emailValue);
+    const eventId=router?.query.id;
+
+    try{
+      const response = await fetch('/api/email-registeration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if(!response.ok)  throw new Error(`Error: ${response.status}`);
+
+      const data = await response.json();
+      console.log(data);
+
+    }
+    
+    catch(e){
+      console.log("ERROR",e)
+    }
+
+  };
+
   data=data[0];
   return (
-    <div>
+    <div className='event_single_page'>
     {console.log('Data')}
       {console.log(data)}
       <h1> {data.title}</h1>
@@ -18,11 +52,11 @@ const SingleEvent = ({data}) => {
         <h2>{data.title}</h2>
         <p>{data.description}</p>
        
-        <form >
+        <form onSubmit={Onsubmit} className='email_registeration'>
 
           <label>Get registered for this event</label>
-          <input type='email' id='email' placeholder='Enetr your email here'></input>
-          <button>Submit</button>
+          <input ref={inputemail} type='email' id='email' placeholder='Enter your email here'></input>
+          <button type='submit'>Submit</button>
 
 
         </form>
